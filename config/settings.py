709b19b5d29beed5c30e5d9a3327015c17bf7091
Henu <hyun0404woo@gmail.com>
2021-07-23 20:01:12 +0900

    allow host *

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-oj@o@^vwf=e0i+$fse%c)*os7$mwd1@^=d&g5@o8la8y#s(j)2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
